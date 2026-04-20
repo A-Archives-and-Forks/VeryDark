ff684f973506ae66b69b767762b8274c65164473
wkbin <wangkebin1997@gmail.com>
2026-04-20 14:07:06 +0800

    version 2.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         minSdk = 31
         targetSdk = 36
         versionCode = 3
-        versionName = "2.2"
+        versionName = "2.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
